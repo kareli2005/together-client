c88247acb21ee8bc745cc08fa75f582df2952cd2
kareli2005 <cotneqareli123@gmail.com>
2024-10-28 13:31:49 +0400

    commit

--- src/containers/home/Settings.jsx
+++ src/containers/home/Settings.jsx
@@ -25,7 +25,7 @@ const Settings = () => {
   return (
     <div className='w-full h-full rounded-xl flex flex-col justify-start items-center gap-4 home-container-enter'>
       <div className='flex flex-col sm:flex-row gap-4 w-full duration-300 justify-center items-center'>
-        <div className='h-[160px] w-[160px] min-h-[160px] min-w-[160px] md:h-[220px] md:w-[220px] md:min-w-[220px] md:min-h-[220px] aspect-square flex rounded-xl overflow-hidden p-2 bg-white duration-300 relative'>
+        <div className='h-[160px] w-[160px] min-h-[160px] min-w-[160px] md:h-[220px] md:w-[220px] md:min-w-[220px] md:min-h-[220px] aspect-square flex rounded-xl overflow-hidden p-2 bg-white duration-300 relative shadow-lg shadow-gray-300'>
           <div className='w-full h-full absolute cursor-pointer duration-300 top-0 left-0 opacity-0 hover:opacity-100 bg-black bg-opacity-10 text-white font-bold text-sm flex flex-col justify-center items-center gap-2'>
             <button onClick={thisFunctionWillAddSoon} className='w-2/3 select-none flex gap-1 border-2 rounded-xl border-[#0074d9] bg-[#0074d9] bg-opacity-60 hover:bg-opacity-100 duration-300 px-2 py-1 justify-center items-center'>
               <PublishedWithChangesIcon/>
@@ -39,7 +39,7 @@ const Settings = () => {
           <img src={userData.profile} alt="/user_profile" className='rounded-xl w-full h-full flex object-cover' />
         </div>
         <div className='flex w-full max-w-2/3 h-full flex-col gap-4 font-bold text-slate-500 text-sm sm:text-base md:text-lg duration-300'>
-          <div className='bg-white px-6 py-4 rounded-xl h-full w-full flex justify-between items-center flex-wrap gap-2'>
+          <div className='bg-white px-6 py-4 rounded-xl h-full w-full flex justify-between items-center flex-wrap gap-2 shadow-lg shadow-gray-300'>
             <p className='flex gap-2 text-center'>
               Email: <span className='text-[#0074d9]'>{userData.email}</span>
             </p>
@@ -48,7 +48,7 @@ const Settings = () => {
               <p>Change</p>
             </button>
           </div>
-          <div className='bg-white px-6 py-4 rounded-xl h-full w-full flex justify-between items-center flex-wrap gap-2'>
+          <div className='bg-white px-6 py-4 rounded-xl h-full w-full flex justify-between items-center flex-wrap gap-2 shadow-lg shadow-gray-300'>
             <p className='flex gap-2 text-center'>
               Username: <span className='text-[#0074d9]'>{userData.username}</span>
             </p>
@@ -59,7 +59,7 @@ const Settings = () => {
           </div>
         </div>
       </div>
-      <div className='w-full gap-4 font-bold text-slate-500 text-sm sm:text-base md:text-lg bg-white rounded-xl py-4 sm:py-6 px-6 flex justify-between items-start duration-300'>
+      <div className='w-full gap-4 font-bold text-slate-500 text-sm sm:text-base md:text-lg bg-white rounded-xl py-4 sm:py-6 px-6 flex justify-between items-start duration-300 shadow-lg shadow-gray-300'>
         <div className='flex gap-4 text-center items-center'>
           <p className='flex gap-2 text-center'>
             Update Password: 
